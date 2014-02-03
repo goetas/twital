22f7cd0c81c7e6e14f847900bbbdf012c958c2f2
Asmir Mustafic <asmir@lignano.it>
2014-02-03 15:24:51 +0100

    block node handling

---
+++ src/Goetas/Twital/Node/BlockCallNode.php
@@ -0,0 +1,24 @@
+<?php
+namespace Goetas\Twital\Node;
+
+use Goetas\Twital\Node;
+use Goetas\Twital\Compiler;
+
+use Goetas\Twital\DOMHelper;
+use Exception;
+class BlockCallNode implements Node
+{
+    function visit(\DOMElement $node, Compiler $twital)
+    {
+        if ($node->hasAttributeNS(Compiler::NS, "name")) {
+            $name = $node->getAttributeNS(Compiler::NS, "name");
+        } elseif ($node->hasAttribute("name")) {
+            $name = "'". $node->getAttributeNS("name"). "'";
+        } else {
+            $name = "'parent'";
+        }
+
+        $txt = $node->ownerDocument->createTextNode("{{ block($name) }}");
+        $node->parentNode->replaceChild($txt, $node);
+    }
+}
