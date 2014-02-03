0df8e4bcdd300508526d41d5d4908836e6d9fee7
Asmir Mustafic <asmir@lignano.it>
2014-02-03 15:27:57 +0100

    code style

--- src/Goetas/Twital/Node/BlockCallNode.php
+++ src/Goetas/Twital/Node/BlockCallNode.php
@@ -3,17 +3,18 @@ namespace Goetas\Twital\Node;
 
 use Goetas\Twital\Node;
 use Goetas\Twital\Compiler;
-
 use Goetas\Twital\DOMHelper;
 use Exception;
+
 class BlockCallNode implements Node
 {
+
     function visit(\DOMElement $node, Compiler $twital)
     {
         if ($node->hasAttributeNS(Compiler::NS, "name")) {
             $name = $node->getAttributeNS(Compiler::NS, "name");
         } elseif ($node->hasAttribute("name")) {
-            $name = "'". $node->getAttributeNS("name"). "'";
+            $name = "'" . $node->getAttributeNS("name") . "'";
         } else {
             $name = "'parent'";
         }
