0376de252d43b9a08fd03934ab33df24451fe251
Asmir Mustafic <goetas@gmail.com>
2014-02-17 00:04:44 +0100

    doc

--- src/Goetas/Twital/Extension.php
+++ src/Goetas/Twital/Extension.php
@@ -4,13 +4,52 @@ namespace Goetas\Twital;
 interface Extension
 {
 
+	/**
+	 * Array of objects implementing Attribute interface, responsible of DOM attribute handling.
+	 * The returned array must be a "two level array", first level as namespace and second level as attribute name.
+	 * Example:
+	 * <code>
+	 * 	array(
+	 * 		'http://www.w3.org/1998/Math/MathML'=>array(
+	 * 			'math'=>MathML\MathAttribute()
+	 * 		)
+	 *  )
+	 * </code>
+	 * @return array
+	 */
     public function getAttributes();
-
+    /**
+     * Array of objects implementing Node interface, responsible of DOM node handling.
+     * The returned array must be a "two level array", first level as namespace and second level as attribute name.
+	 * Example:
+	 * <code>
+	 * 	array(
+	 * 		'http://www.w3.org/1998/Math/MathML'=>array(
+	 * 			'math'=>MathML\MathAttribute()
+	 * 		)
+	 *  )
+	 * </code>
+	 *
+     * @return array
+     */
     public function getNodes();
 
+    /**
+     * Array of callbacks that runs after DOM compilation process.
+     * @return array
+     */
     public function getPostFilters();
 
+    /**
+     * Array of callbacks that runs before DOM compilation process (after template loading).
+     * @return array
+     */
     public function getPreFilters();
 
+	/**
+     * Array of objects implementing {DOMLoader} interface, responsible of DOM loading and dumping.
+     * The key of
+	 * @return array
+	 */
     public function getDOMLoaders();
 }
