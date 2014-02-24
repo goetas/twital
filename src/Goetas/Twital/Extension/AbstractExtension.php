cd7254af7f66306d6f971489e3168f4b6b54bc2a
Asmir Mustafic <goetas@gmail.com>
2014-02-24 22:54:08 +0100

    ref

---
+++ src/Goetas/Twital/Extension/AbstractExtension.php
@@ -0,0 +1,38 @@
+<?php
+namespace Goetas\Twital\Extension;
+
+use Goetas\Twital\Extension;
+
+class AbstractExtension implements Extension
+{
+
+    public function getAttributes()
+    {
+
+    }
+
+    public function getNodes()
+    {
+
+    }
+
+    public function getPostFilters()
+    {
+
+    }
+
+    public function getPreFilters()
+    {
+
+    }
+
+    public function getLoaders()
+    {
+
+    }
+
+    public function getDumpers()
+    {
+
+    }
+}
