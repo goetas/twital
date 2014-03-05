9009a36512454254fc7f6efd668ae58df134f174
Asmir Mustafic <goetas@gmail.com>
2014-03-05 10:50:15 +0100

    migliorato embed node

--- src/Goetas/Twital/Node/EmbedNode.php
+++ src/Goetas/Twital/Node/EmbedNode.php
@@ -9,25 +9,37 @@ use Goetas\Twital\Twital;
 
 class EmbedNode implements Node
 {
+
     public function visit(\DOMElement $node, CompilationContext $context)
     {
-        if (! $node->hasAttribute("from") && ! $node->hasAttributeNS(Twital::NS,"from")) {
+        if ($node->hasAttribute("from-exp")) {
+            $filename = $node->getAttribute("from-exp");
+        } elseif ($node->hasAttribute("from-exp")) {
+            $filename = "'" . $node->getAttribute("from") . "'";
+        } else {
             throw new Exception("name or name-exp attribute is required");
         }
 
         // remove any non-element node
-        foreach (iterator_to_array($node->childNodes) as $child){
-            if(!($child instanceof \DOMElement)){
+        foreach (iterator_to_array($node->childNodes) as $child) {
+            if (! ($child instanceof \DOMElement)) {
                 $child->parentNode->removeChild($child);
             }
         }
-        $filename = $node->hasAttributeNS(Twital::NS, "from") ? $node->getAttributens(CompilationContext::NS, "from") : ("'" . $node->getAttribute("from") . "'");
+
         $context->compileChilds($node);
 
         $code = "embed {$filename}";
-        $code .= $node->getAttribute("ignore-missing") ? " ignore missing" : "";
-        $code .= $node->hasAttribute("with") ? (" with " . $node->getAttribute("with")) : "";
-        $code .= $node->getAttribute("only") == "true" ? " only " : "";
+
+        if ($node->hasAttribute("ignore-missing") && $node->hasAttribute("ignore-missing") !== false) {
+            $code .= " ignore missing";
+        }
+        if ($node->hasAttribute("with")) {
+            $code .= " with " . $node->getAttribute("with");
+        }
+        if ($node->hasAttribute("only") && $node->getAttribute("only") !== "false") {
+            $code .= " ignore missing";
+        }
 
         $ext = $context->createControlNode($code);
 
@@ -35,6 +47,5 @@ class EmbedNode implements Node
         array_unshift($set, $ext);
 
         DOMHelper::replaceWithSet($node, $set);
-
     }
 }
