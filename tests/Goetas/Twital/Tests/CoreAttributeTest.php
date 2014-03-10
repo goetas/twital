9e1d9bf7754e13eb5fac34d911c2aec4da8df076
Asmir Mustafic <goetas@gmail.com>
2014-03-10 09:07:28 +0100

    testys

--- tests/Goetas/Twital/Tests/CoreAttributeTest.php
+++ tests/Goetas/Twital/Tests/CoreAttributeTest.php
@@ -40,18 +40,6 @@ class CoreAttributeTest extends \PHPUnit_Framework_TestCase
 
             array('<div t:content="foo">content</div>', '<div>{{ foo }}</div>'),
 
-            array('<t:include from-exp="foo"/>', '{% include foo %}'),
-            array('<t:include from="aaa.html"/>', '{% include "aaa.html" %}'),
-            array('<t:include from="aaa.html" ignore-missing="true"/>', '{% include "aaa.html" ignore missing %}'),
-            array('<t:include from="aaa.html" sandboxed="true"/>', '{% include "aaa.html" sandboxed = true %}'),
-            array('<t:include from="aaa.html" with="{aaa:bbb}"/>', '{% include "aaa.html" with {aaa:bbb} %}'),
-            array('<t:include from="aaa.html" with="{aaa:bbb} only"/>', '{% include "aaa.html" with {aaa:bbb} only %}'),
-            array('<t:include from="aaa.html" with="{aaa:bbb}" only="true"/>', '{% include "aaa.html" with {aaa:bbb} only %}'),
-
-            array('<t:omit>foo</t:omit>', 'foo'),
-            array('<t:omit><div>foo</div></t:omit>', '<div>foo</div>'),
-
-            array('<t:include from-exp="foo"/>', '{% include foo %}'),
         );
     }
 }
---
+++ tests/Goetas/Twital/Tests/CoreNodesTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Goetas\Twital\Tests;
+
+use Goetas\Twital\Twital;
+use Goetas\Twital\SourceAdapter\XMLAdapter;
+
+
+class CoreNodesTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->twital = new Twital();
+        $this->sourceAdapter = new XMLAdapter();
+    }
+
+    /**
+     * @dataProvider getData
+     */
+    public function testVisitNode($source, $expected)
+    {
+        $compiled = $this->twital->compile($this->sourceAdapter, $source);
+        $this->assertEquals($expected, $compiled);
+    }
+
+    public function getData()
+    {
+        return array(
+
+            array('<t:include from-exp="foo"/>', '{% include foo %}'),
+            array('<t:include from="aaa.html"/>', '{% include "aaa.html" %}'),
+            array('<t:include from="aaa.html" ignore-missing="true"/>', '{% include "aaa.html" ignore missing %}'),
+            array('<t:include from="aaa.html" sandboxed="true"/>', '{% include "aaa.html" sandboxed = true %}'),
+            array('<t:include from="aaa.html" with="{aaa:bbb}"/>', '{% include "aaa.html" with {aaa:bbb} %}'),
+            array('<t:include from="aaa.html" with="{aaa:bbb} only"/>', '{% include "aaa.html" with {aaa:bbb} only %}'),
+            array('<t:include from="aaa.html" with="{aaa:bbb}" only="true"/>', '{% include "aaa.html" with {aaa:bbb} only %}'),
+
+            array('<t:omit>foo</t:omit>', 'foo'),
+            array('<t:omit><div>foo</div></t:omit>', '<div>foo</div>'),
+
+            array('<t:include from-exp="foo"/>', '{% include foo %}'),
+        );
+    }
+}
+
+
