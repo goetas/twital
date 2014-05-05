fcdac6ccccbe4e6695a730c8a7048b8ab905083b
Asmir Mustafic <goetas@gmail.com>
2014-05-05 08:58:43 +0200

    Event tests

---
+++ tests/Goetas/Twital/Tests/Event/SourceEventTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace Goetas\Twital\Tests\Event;
+
+use Goetas\Twital\Twital;
+use Goetas\Twital\EventDispatcher\SourceEvent;
+
+class SourceEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->twital = new Twital();
+    }
+
+    public function testBase()
+    {
+
+        $template = md5(microtime());
+        $ist = new SourceEvent($this->twital, $template);
+
+        $this->assertSame($this->twital, $ist->getTwital());
+        $this->assertSame($template, $ist->getTemplate());
+    }
+
+    public function testBaseSetter()
+    {
+        $template = md5(microtime());
+        $ist = new SourceEvent($this->twital, $template);
+
+        $this->assertSame($template, $ist->getTemplate());
+
+        $templateNew = md5(microtime());
+        $ist->setTemplate($templateNew);
+
+        $this->assertSame($templateNew, $ist->getTemplate());
+    }
+}
+
+
---
+++ tests/Goetas/Twital/Tests/Event/TemplateEventTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Goetas\Twital\Tests\Event;
+
+use Goetas\Twital\Twital;
+use Goetas\Twital\EventDispatcher\TemplateEvent;
+
+class TemplateEventTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->twital = new Twital();
+        $this->template = $this->getTemplateMock();
+    }
+
+    protected function getTemplateMock()
+    {
+        return $this->getMock('Goetas\Twital\Template', null, array(new \DOMDocument('1.0', 'UTF-8')));
+    }
+
+    public function testBase()
+    {
+        $ist = new TemplateEvent($this->twital, $this->template);
+
+        $this->assertSame($this->twital, $ist->getTwital());
+        $this->assertSame($this->template, $ist->getTemplate());
+    }
+
+    public function testBaseSetter()
+    {
+        $ist = new TemplateEvent($this->twital, $this->template);
+
+        $template = $this->getTemplateMock();
+        $ist->setTemplate($template);
+
+        $this->assertSame($template, $ist->getTemplate());
+    }
+}
+
+
