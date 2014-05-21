d33caf611e53bd090a975215d41564c900550c5d
Martin Hasoň <martin.hason@gmail.com>
2014-05-21 16:12:48 +0200

    Fixed loading xml source and improved error messages

--- src/Goetas/Twital/SourceAdapter/XMLAdapter.php
+++ src/Goetas/Twital/SourceAdapter/XMLAdapter.php
@@ -16,11 +16,7 @@ class XMLAdapter implements SourceAdapter
      */
     public function load($source)
     {
-        $dom = new \DOMDocument('1.0', 'UTF-8');
-
-        if (! @$dom->loadXML($source)) {
-            throw new \Exception("Error during XML conversion into DOM");
-        }
+        $dom = $this->createDom($source);
 
         return new Template($dom, $this->collectMetadata($dom, $source));
     }
@@ -58,5 +54,44 @@ class XMLAdapter implements SourceAdapter
             }
             return $source;
         }
+    }
+
+    protected function createDom($source)
+    {
+        $internalErrors = libxml_use_internal_errors(true);
+        $disableEntities = libxml_disable_entity_loader(true);
+        libxml_clear_errors();
+
+        $dom = new \DOMDocument('1.0', 'UTF-8');
+        if (!$dom->loadXML($source, LIBXML_NONET | (defined('LIBXML_COMPACT') ? LIBXML_COMPACT : 0))) {
+            libxml_disable_entity_loader($disableEntities);
+
+            throw new \InvalidArgumentException(implode("\n", $this->getXmlErrors($internalErrors)));
+        }
+
+        libxml_use_internal_errors($internalErrors);
+        libxml_disable_entity_loader($disableEntities);
+
+        return $dom;
+    }
+
+    protected function getXmlErrors($internalErrors)
+    {
+        $errors = array();
+        foreach (libxml_get_errors() as $error) {
+            $errors[] = sprintf('[%s %s] %s (in %s - line %d, column %d)',
+                LIBXML_ERR_WARNING == $error->level ? 'WARNING' : 'ERROR',
+                $error->code,
+                trim($error->message),
+                $error->file ? $error->file : 'n/a',
+                $error->line,
+                $error->column
+            );
+        }
+
+        libxml_clear_errors();
+        libxml_use_internal_errors($internalErrors);
+
+        return $errors;
     }
 }
