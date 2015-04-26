a779007768a12c56fb83e72e8270b4316e0c9dfe
Asmir Mustafic <goetas@gmail.com>
2015-04-26 20:48:19 +0200

    Ensure to have alvays a valid variable name

--- src/Goetas/Twital/Attribute/AttrAttribute.php
+++ src/Goetas/Twital/Attribute/AttrAttribute.php
@@ -16,7 +16,7 @@ class AttrAttribute implements Attribute
 {
     public static function getVarname(\DOMElement $node)
     {
-        return "__a9" . strtr($node->getAttributeNS(Twital::NS, '__internal-id__').spl_object_hash($node), "-","_");
+        return "__a" . sha1($node->getAttributeNS(Twital::NS, '__internal-id__').spl_object_hash($node));
     }
 
     public function visit(\DOMAttr $att, Compiler $context)
